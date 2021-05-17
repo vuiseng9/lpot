7de2768938ae318c00a0ae43678c5fb6705b3785
Vui Seng Chua <vui.seng.chua@intel.com>
2021-05-17 08:22:34 -0700

    add ptq resnet50 imagenet config and runner for basic and autoq strategy

---
+++ examples/pytorch/image_recognition/imagenet/cpu/ptq/run_ptq_resnet50_autoq.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export LOGLEVEL=DEBUG
+WORKDIR=/path/to/lpot/examples/pytorch/image_recognition/imagenet/cpu/ptq
+
+cd ${WORKDIR}
+
+ARCH=resnet50
+
+nohup python main.py \
+    --pretrained \
+    -a $ARCH \
+    -b 30 \
+    -e -t \
+    --tuned_checkpoint ${ARCH}_autoq_run \
+    --conf ./conf.${ARCH}.autoq.yaml \
+    /data/dataset/imagenet/ilsvrc2012/torchvision 2>&1 | tee log.${ARCH}.autoq &
